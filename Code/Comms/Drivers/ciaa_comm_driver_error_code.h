30990165c367f15e65f428b783a078612ae06235
Alvaro <denisacostaq@gmail.com>
2014-04-24 00:33:28 -0400

    corregido el codgico desde experimental_cmake

--- Code/Comms/Drivers/ciaa_comm_driver_error_code.h
+++ Code/Comms/Drivers/ciaa_comm_driver_error_code.h
@@ -10,17 +10,10 @@
     \attention <h1><center>&copy; COPYRIGHT
     GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007</center></h1>
 
-    \copyright
-
-    <h3>
-      This file is part of
-      <a class="el" href="http://proyecto-ciaa.com.ar">
-        <h3>
-          CIAA project (Computadora Industrial Argentina Abierta).
-        </h3>
-      </a>
-    </h3>
-    Copyright (C) 2014 $(Entidad que patenta)
+    <a class="el" href="http://proyecto-ciaa.com.ar">
+      This file is part of CIAA Project.
+      ==================================
+    </a>
 
   This software is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -44,20 +37,20 @@
  * \brief The CommDriverErrorCode enum
  * \ingroup Drivers
  */
-enum class CommDriverErrorCode
-{
-  Ok,                          /**< General operation without errors. */
-  access_error,                /**< Application lacked the required privileges. */
-  resource_error,              /**< The local system ran out of resources (e.g., too many fd). */
-  connection_error,            /**< The connect operation fail or the current connection has been broken. */
-  inuse_error,                 //TODO<denisacostaq\@gmail.com>               /**< The device is busy. */
-  unsupported_operation_error, //TODO<denisacostaq\@gmail.com>: ej escribir un dispositivo de solo lectura
-  unfinished_operation_error,  /**< The last operation attempted has not finished yet (still in progress in the background). */
-  temporary_error,             /**< You can retry the operation later. */
-  disconnect_error,            /**< Could not disconnect the device. */
-  read_error,                   /**< The read operation has been fail. */
-  write_error,                  /**< The write operation has been fail. */
-  timeout_error,                /**< The operation fail by timeout. */
-  unknown_error                /**< An unidentified error occurred. */
+enum class CommDriverErrorCode {
+  without_error,         /**< General operation without errors. */
+  access,                /**< Application lacked the required privileges. */
+  resource, /**< The local system ran out of resources (e.g., too many fd). */
+  connection, /**< The connect operation fail or current connection broken. */
+  inuse,                 /**< The device is busy. */
+  unsuported_operation,  // TODO<denisacostaq\@gmail.com>: ej escribir un
+                         // dispositivo de solo lectura
+  unfinished_operation,  /**< Still in progress in the background. */
+  temporary,             /**< You can retry the operation later. */
+  disconnect,            /**< Could not disconnect the device. */
+  read,                  /**< The read operation has been fail. */
+  write,                 /**< The write operation has been fail. */
+  timeout,               /**< The operation fail by timeout. */
+  unknown                /**< An unidentified error occurred. */
 };
-#endif // COMM_DRIVER_ERROR_CODE_H
+#endif  // COMM_DRIVER_ERROR_CODE_H
